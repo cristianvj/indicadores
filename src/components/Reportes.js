0c2300171cb35e05c73710bb3e28d267a42317d8
cristianvj <cristiandavid.vj@gmail.com>
2021-09-15 09:12:16 -0500

    colores de table

--- src/components/Reportes.js
+++ src/components/Reportes.js
@@ -33,60 +33,44 @@ const Reportes = () => {
                     <tr>
                         <th scope="col">Eje ↑</th>
                         <th scope="col">Area ↑</th>
-                        <th scope="col">Perspectiva ↑</th>
+                        <th scope="col">Perspectiva↑</th>
                         <th scope="col">Estrategia ↑</th>
                         <th scope="col">Objetivo ↑</th>
                         <th scope="col">Indicador ↑</th>
-                        <th scope="col">Meta ↑</th>
-                        <th scope="col">Cumplimiento ↑</th>
+                        <th scope="col">Meta↑</th>
+                        <th scope="col">Resultado↑</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
+                    <tr className="table-success">
+                        <td scope="row">Mallamas EPS Responsable</td>
+                        <td>Atención al usuario</td>
+                        <td>Cliente</td>
+                        <td>dialogo sin barreras para un buen vivir</td>
+                        <td>Aumentar el nivel de satisfacción de nuestros usuarios, respetando las particularidades </td>
+                        <td>Porcentaje de cumplimiento de implementación y funcionamiento del modelo de atención </td>
+                        <td>50%</td>
+                        <th>63%</th>
                     </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td><td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
+                    <tr className="table-danger">
+                        <td scope="row">Mallamas EPS Responsable</td>
+                        <td>Direccion de salud</td>
+                        <td>Financiera</td>
+                        <td>Tejiendo el modelo propio e intercultural de atención en salud para el buen vivir</td>
+                        <td>Diseñar e implementar modelos de atención integral en salud,   para garantizar acceso </td>
+                        <td>% De cumplimiento de la implementación</td>
+                        <td>50%</td>
+                        <th>25%</th>
                     </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td><td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td><td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td><td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
+                    <tr className="table-warning">
+                        <td scope="row">Mallamas EPS Responsable</td>
+                        <td>Direccion de salud</td>
+                        <td>Proceso Interno</td>
+                        <td>Planear y Racionalizar Costo Médico</td>
+                        <td>Diseñar , implementar y evaluar un  modelo de contratación de red prestadora y proveedores, efectivo para la EPS-I</td>
+                        <td>% de legalización de contratos en tiempo oportuno y con presupuesto definido</td>
+                        <td>90%</td>
+                        <th>61%</th>
                     </tr>
                     </tbody>
                 </table>
